c6dd8aa1b2efa05be57197378d49fc9c12892ece
Tarek Mamdouh <egypty3@gmail.com>
2024-08-09 21:14:52 +0300

    Add project files.

---
+++ HTML_CSS_Lect1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HTML_CSS_Lect1.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
