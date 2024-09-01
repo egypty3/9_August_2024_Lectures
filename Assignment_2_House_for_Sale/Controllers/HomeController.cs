00c923c970422897ecf093c23d0e110b8381a0fd
Tarek <egypty3@gmail.com>
2024-09-01 13:40:29 +0300

    + Assignment solution

---
+++ Assignment_2_House_for_Sale/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Assignment_2_House_for_Sale.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment_2_House_for_Sale.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
