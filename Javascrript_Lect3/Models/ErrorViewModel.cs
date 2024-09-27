c46adb595608f01052205dd0f11a97bf31173a0a
Tarek Mamdouh <egypty3@gmail.com>
2024-09-27 19:34:00 +0300

    + styling a table cells background by user choices

---
+++ Javascrript_Lect3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Javascrript_Lect3.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
