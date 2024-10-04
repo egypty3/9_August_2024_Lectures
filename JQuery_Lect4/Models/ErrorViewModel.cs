ac3ac1bfaa7d39ea4eb9bc2118deaade2356fc34
Tarek Mamdouh <egypty3@gmail.com>
2024-10-04 19:31:14 +0300

    +Hello  Jquery

---
+++ JQuery_Lect4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JQuery_Lect4.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ JQuery_Lect4/myJQuery/helloJQuery.js
@@ -0,0 +1,26 @@
+﻿$(
+       function () {
+
+                // depricated way
+                //$('input').click(
+                //    function () {
+                //        $('body').css('background-color', 'yellow');
+                //        $('#hh').hide(1000);
+                //    }
+                //);
+
+        $('input').on('click',
+            function () {
+                $('body').css('background-color', 'yellow');
+                $('#hh').hide(1000);
+            });
+
+        $('p').on('click',
+            function () {
+                $(this).hide(1000);
+            }
+        );
+
+    }
+
+);
