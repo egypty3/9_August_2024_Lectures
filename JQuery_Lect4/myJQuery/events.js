4f5ab48c621040ee1026d7688efb0ed2e274d8db
Tarek Mamdouh <egypty3@gmail.com>
2024-10-04 21:32:13 +0300

    + selectors + events

---
+++ JQuery_Lect4/myJQuery/events.js
@@ -0,0 +1,75 @@
+﻿$(
+    function () {
+
+        $('#p1').on(
+            {
+                'mouseenter':
+                function () {
+                    $('#messages').text('you enter the paragraph with an id of #p1');
+                }
+            },
+            {
+                'mouseleave':
+                function() {
+                    $('#messages').text('Hatwhashna !!!');
+                }
+
+            }
+        );
+
+
+        $('#p2').on(
+            {
+                'mousedown':
+                function (eventData) {
+                    $('#messages').text(
+                        `mouse down on  #p2 with button ${eventData.button}`
+                    );
+                }
+            },
+            {
+                'mouseup':
+                function(eventData) {
+                    $('#messages').text(
+                        `mouse up on  #p2 with button ${eventData.button}`);
+                }
+            }
+        );       
+
+        //$('input[type="text"]').on('focus',
+        //    function(){
+        //        $(this).css('background-color', 'hotpink');
+
+        //    }
+        //);
+
+        //$('input[type="text"]').on('blur',
+        //    function () {
+        //        $(this).css('background-color', 'white');
+
+        //    }
+        //);
+
+        $('input[type="text"]').on(
+            {
+                'focus':
+                function() {
+                    $(this).css('background-color', 'hotpink');
+
+                }
+            },
+
+            {
+                'blur':
+                function() {
+                    $(this).css('background-color', 'white');
+
+                }
+            }
+        );
+
+       
+
+    }
+
+);
