c841f3215c3b6cc80d0f3fedf11cca8a4e5278f5
Tarek Mamdouh <egypty3@gmail.com>
2024-10-18 21:19:51 +0300

    + JQuery effects + JQuery animations + JQuery forms

---
+++ JQuery_Lect5/myJQuery/jqueryForms.js
@@ -0,0 +1,49 @@
+﻿$(function () {
+    $("#sampleForm").on('submit',
+        function (e) {
+
+            var isValid = true;
+            e.preventDefault();
+
+            $('input,select').removeClass('error');
+
+            //Validate name field
+            if ($('#name').val().trim() == '') {
+                isValid = false;
+
+                $('#name').addClass('error');
+            }
+
+            //Validate email field
+            if ($('#email').val().trim() == '') {
+                isValid = false;
+
+                $('#email').addClass('error');
+            }
+            //Validate gender field
+            if ($('#gender').val() == '') {
+                isValid = false;
+
+                $('#gender').addClass('error');
+            }
+
+            //Validate age field
+            if ($('#age').val() == '') {
+                isValid = false;
+
+                $('#age').addClass('error');
+            }
+
+            //Validate phone field
+            if ($('#phone').val() == '') {
+                isValid = false;
+
+                $('#phone').addClass('error');
+            }
+
+            if (isValid) {
+                alert('Form submitted successfully');
+            }
+        }
+    );
+});
