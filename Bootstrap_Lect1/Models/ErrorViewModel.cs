45e147933cbc8faf6f1c5095f514ab805ee73c8b
Tarek Mamdouh <egypty3@gmail.com>
2024-11-08 23:14:35 +0200

    + Bootstrap Utilities

---
+++ Bootstrap_Lect1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bootstrap_Lect1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
