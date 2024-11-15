9eb51be3c495f59b05bac3a94ba350d9c9b2dc95
Tarek Mamdouh <egypty3@gmail.com>
2024-11-15 21:31:45 +0200

    + Grid system + Form component

---
+++ Bootstrap_Lect2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bootstrap_Lect2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
