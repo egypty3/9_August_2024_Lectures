1763663c27bd023258a4d1af3c05311c4ad31c60
Tarek Mamdouh <egypty3@gmail.com>
2024-11-29 21:36:39 +0200

    +Cards +Carousel +DropDown +ButtonGroup +ListGroup +Badge +Navbar

---
+++ Bootstrap_Lect3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bootstrap_Lect3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
