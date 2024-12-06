2f8a71e38e611c15f84a70e0c3790155a6724e08
Tarek Mamdouh <egypty3@gmail.com>
2024-12-06 19:24:53 +0200

    + blogen web site initilaization and libraries update + menu (navbar done)

---
+++ Bootstrap_Lect4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bootstrap_Lect4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
