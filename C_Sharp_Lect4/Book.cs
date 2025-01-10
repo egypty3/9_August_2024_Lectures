af6318831d4c0034f9a638668e9a4d038b1093cb
Tarek Mamdouh <egypty3@gmail.com>
2025-01-10 18:49:58 +0200

    + Structure example

---
+++ C_Sharp_Lect4/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lect4
+{
+   struct Book
+    {
+      public  int book_id;
+      public  string title;
+      public  string author;
+      public  string subject;
+    }
+}
---
+++ C_Sharp_Lect4/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace C_Sharp_Lect4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // StructuresExample();
+            EnumExample();
+        }
+
+        static void StructuresExample()
+        {
+            Book book1 = new Book(); // Declare Book1 of type Book
+            Book book2 = new Book(); // Declare Book2 of type Book
+
+            // book1 specification
+            book1.book_id = 6495407;
+            book1.author = "Abu Adel";
+            book1.title = "learn C# in 24 hours";
+            book1.subject = "C# Programming Tutorial";
+
+
+            // book2 specification
+            book2.book_id = 6495700;
+            book2.author = "Noha Ali";
+            book2.title = "El Bacaloria El Gedida";
+            book2.subject = "Egyptian Bacaloria";
+
+            // print Book1 info
+            Console.WriteLine("Book 1 ID : {0}", book1.book_id);
+            Console.WriteLine("Book 1 Title : {0}", book1.title);
+            Console.WriteLine("Book 1 Author : {0}", book1.author);
+            Console.WriteLine("Book 1 Subject : {0}", book1.subject);
+          
+        }
+
+        static void EnumExample()
+        {
+         
+        }
+    }
+}
