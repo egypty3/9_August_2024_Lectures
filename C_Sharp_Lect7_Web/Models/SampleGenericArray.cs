a52ae3fb8ea5732e4ffc4a581a3970e9c887e093
Tarek Mamdouh <egypty3@gmail.com>
2025-02-07 18:55:34 +0200

    + SampleGenericArray class definition

---
+++ C_Sharp_Lect7_Web/Models/SampleGenericArray.cs
@@ -0,0 +1,25 @@
+﻿namespace C_Sharp_Lect7_Web.Models
+{
+    public class SampleGenericArray<T>
+    {
+        private T[] array;
+
+        // Constructor - create an array of the specified size
+        public SampleGenericArray(int size)
+        {
+            array = new T[size];
+        }
+
+        // getMyItem - return the item at the specified index
+        public T getMyItem(int index)
+        {
+            return array[index];
+        }
+
+        // setMyItem - set the item at the specified index
+        public void setMyItem(int index, T value)
+        {
+            array[index] = value;
+        }
+    }
+}
