92c3d429d9272a532e5ba2cb9bca99f0f095a383
Tarek Mamdouh <egypty3@gmail.com>
2025-02-07 21:43:00 +0200

    + hanlding Files using  System.IO classes + completed the Controller and View code at the web application

--- C_Sharp_Lect7_Web/Controllers/HomeController.cs
+++ C_Sharp_Lect7_Web/Controllers/HomeController.cs
@@ -17,6 +17,89 @@ namespace C_Sharp_Lect7_Web.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Index(string theType,int noOfElements, string theOrder)
+        {
+            ViewBag.noOfElements = noOfElements;
+            ViewBag.theType = theType;
+
+            // the button clicked was 'create'
+            if (theOrder == "create")
+            {
+                ViewBag.theOrder = "create";
+            }
+            // the button clicked was 'save'
+            else if (theOrder == "save")
+            {
+                ViewBag.theOrder = "save";
+                List<string> elements = new List<string>();
+
+                for (int i = 0; i < noOfElements; i++)
+                {
+                    string elementName = $"element_{i}";
+                    string elementValue = Request.Form[elementName];
+                    elements.Add(elementValue);
+                }
+
+                // Process the elements list as needed
+                string resultToView = "";
+
+                if (theType == "int")
+                {
+                    SampleGenericArray<int> intArray =
+                        new SampleGenericArray<int>(noOfElements);
+
+                    // Setting up values
+                    for (int i = 0; i < noOfElements; i++)
+                    {
+                        intArray.setMyItem(i, int.Parse(elements[i]));
+                    }
+
+                    // Retreiving the values
+                    for (int i = 0; i < noOfElements; i++)
+                    {
+                        resultToView += intArray.getMyItem(i) + "<br/>";
+                    }
+
+                }
+                else if (theType == "char")
+                {
+                    SampleGenericArray<char> intArray =
+                       new SampleGenericArray<char>(noOfElements);
+
+                    // Setting up values
+                    for (int i = 0; i < noOfElements; i++)
+                    {
+                        intArray.setMyItem(i, char.Parse(elements[i]));
+                    }
+
+                    // Retreiving the values
+                    for (int i = 0; i < noOfElements; i++)
+                    {
+                        resultToView += intArray.getMyItem(i) + "<br/>";
+                    }
+                }
+
+                ViewBag.resultToView = resultToView;
+
+                // Save resultToView to a file
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), "myArray.txt");
+                System.IO.File.WriteAllText(filePath, resultToView.Replace("<br/>", "\r\n"));
+            }
+            // the button clicked was 'load'
+            else if (theOrder == "load")
+            {
+                ViewBag.theOrder = "load";
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), "myArray.txt");
+                string resultToView = System.IO.File.ReadAllText(filePath);
+                ViewBag.noOfElements = resultToView.Split("\r\n").Count();
+                ViewBag.resultToView = resultToView.Replace("\r\n","<br/>");
+
+            }
+
+
+                return View();
+        }
 
         public IActionResult Privacy()
         {
