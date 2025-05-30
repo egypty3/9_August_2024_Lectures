19d82c5172c72ac07c459e494ca5dfe76e464371
Tarek Mamdouh <egypty3@gmail.com>
2025-05-30 20:54:24 +0300

    Lecture 4

---
+++ ASP.NET Core Lect4/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP.NET_Core_Lect1.Models
+{
+    //[Table("ElDawrat")]
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }       
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        // Navigation properties
+        public ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+    }
+}
---
+++ ASP.NET Core Lect4/Models/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿namespace ASP.NET_Core_Lect1.Models
+{
+    public enum Grade
+    {
+        A,B,C,D,E,F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        public Grade? Grade { get; set; }
+
+        // Navigation properties
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
